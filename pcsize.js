66410041a302176daf82a362ffc17c0858fc0e6f
textzero <93716761+textzero@users.noreply.github.com>
2021-11-06 12:43:13 +0800

    mobile and pc update

---
+++ pcsize.js
@@ -0,0 +1,5 @@
+setInterval(function() {
+    if (window.innerWidth < 1234) {
+        document.getElementById("bilibujia").style.display = "flex";
+    } else { document.getElementById("bilibujia").style.display = "none"; }
+});
