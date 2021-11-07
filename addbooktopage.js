34fc59aed8181de90f3d99a9ead30cbffb212046
textzero <93716761+textzero@users.noreply.github.com>
2021-11-07 21:13:25 +0800

    Add files via upload

--- addbooktopage.js
+++ addbooktopage.js
@@ -16,20 +16,36 @@ const jsonurl = "https://raw.githubusercontent.com/textzero/textzero.github.io/m
 $.getJSON(jsonurl, function(data) {
     jsonmian = data;
 }).done(function() {
-    for (var i = 0; i < jsonmian["books"].length; i++) {
-        book_id = jsonmian["books"][i].id;
-        book_name = jsonmian["books"][i].name;
-        book_author = jsonmian["books"][i].auth;
-        book_classify = jsonmian["books"][i].class;
-        $("#booklist").append(`
-        <div class="book" id="id_${book_id}">
-        <font class="booki">书名: </font>
-        <font class="Bookinfo">${book_name}</font>
-        <font class="booki"> | 作者: </font>
-        <font class="Bookinfo">${book_author}</font>
-        <font class="booki"> | 分类: </font>
-        <font class="Bookinfo">${book_classify}</font>
-        <button class="click" title="阅读" onclick="top.location='read.html?book=${book_id}'"><span class="material-icons-round s">auto_stories</span></button></div>
+    if (window.innerWidth <= 1234) {
+
+        for (var i = 0; i < jsonmian["books"].length; i++) {
+            book_id = jsonmian["books"][i].id;
+            book_name = jsonmian["books"][i].name;
+            book_author = jsonmian["books"][i].auth;
+            book_classify = jsonmian["books"][i].class;
+            $("#booklist").append(`
+            <div class="book" id="id_${book_id}">
+            <font class="booki">书名: </font>
+            <font class="Bookinfo">${book_name}</font>
+            <button class="click" title="阅读" onclick="top.location='read.html?book=${book_id}'"><span class="material-icons-rounds">auto_stories</span></button></div>
         `);
+        }
+    } else {
+        for (var i = 0; i < jsonmian["books"].length; i++) {
+            book_id = jsonmian["books"][i].id;
+            book_name = jsonmian["books"][i].name;
+            book_author = jsonmian["books"][i].auth;
+            book_classify = jsonmian["books"][i].class;
+            $("#booklist").append(`
+            <div class="book" id="id_${book_id}">
+            <font class="booki">书名: </font>
+            <font class="Bookinfo">${book_name}</font>
+            <font class="booki"> | 作者: </font>
+            <font class="Bookinfo">${book_author}</font>
+            <font class="booki"> | 分类: </font>
+            <font class="Bookinfo">${book_classify}</font>
+            <button class="click" title="阅读" onclick="top.location='read.html?book=${book_id}'"><span class="material-icons-rounds">auto_stories</span></button></div>
+        `);
+        }
     }
 })
